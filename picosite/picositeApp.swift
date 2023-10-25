1d5278b7490d8f7e644bdbb9f0c20663ab15c187
manorajesh <mano@raje.sh>
2023-10-25 11:52:02 -0700

    Initial Commit

---
+++ picosite/picositeApp.swift
@@ -0,0 +1,17 @@
+//
+//  picositeApp.swift
+//  picosite
+//
+//  Created by Mano Rajesh on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct picositeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
